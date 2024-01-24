79d00b8c1900b5186a1bbfa88e8a596105ce23a3
juanramirez18 <juanramirez189@hotmail.com>
2024-01-23 23:19:06 -0500

    CORS URL

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000
 connectDb()
 const dominios = ['http://localhost:5173/'];
 var corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'https://playful-moonbeam-e888a7.netlify.app',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(express.json())
