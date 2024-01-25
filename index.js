cb385face0c8743aa5c60c2753d5342994f05b0b
juanramirez18 <juanramirez189@hotmail.com>
2024-01-25 16:55:55 -0500

    CORS third

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ var corsOptions = {
   }
 app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Origin", "https://helpful-shortbread-e3f1c3.netlify.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
