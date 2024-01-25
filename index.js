3c54c31a1567fc38c2042671b5cf9ae85522f637
juanramirez18 <juanramirez189@hotmail.com>
2024-01-25 17:16:26 -0500

    CORS try

--- index.js
+++ index.js
@@ -8,17 +8,8 @@ const app = express()
 const port = process.env.PORT || 4000
 
 connectDb()
-const dominios = ['http://localhost:5173/'];
-var corsOptions = {
-    origin: ['https://helpful-shortbread-e3f1c3.netlify.app', 'https://helpful-shortbread-e3f1c3.netlify.app/login']
-  }
+
 app.use(express.json())
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://helpful-shortbread-e3f1c3.netlify.app");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
 app.use("/api/veterinarios", routerVeterinarios)
 app.use("/api/pacientes", routerPacientes)
 app.set('view engine', 'pug')
