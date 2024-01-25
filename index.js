7c475bef4fb3befe44f3927c79c75f37006d828d
juanramirez18 <juanramirez189@hotmail.com>
2024-01-25 16:43:31 -0500

    CORS options

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var corsOptions = {
   }
 app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
