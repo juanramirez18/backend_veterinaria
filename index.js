28dce071afc0d51399df0f20f0f8642b685b03fd
juanramirez18 <juanramirez189@hotmail.com>
2024-01-25 10:02:58 -0500

    chatch error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000
 connectDb()
 const dominios = ['http://localhost:5173/'];
 var corsOptions = {
-    origin: 'https://helpful-shortbread-e3f1c3.netlify.app',
+    origin: 'http://localhost:5173',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(express.json())
