f5ab06172f8a440d708c770d10af7395e0d23ee6
juanramirez18 <juanramirez189@hotmail.com>
2024-01-26 18:13:43 -0500

    CORS solved

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ const port = process.env.PORT || 4000
 connectDb()
 
 app.use(express.json())
-app.use(cors({
-    origin: "*"
-}))
+const dominiosPermitidos = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      // El Origen del Request esta permitido
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use("/api/veterinarios", routerVeterinarios)
 app.use("/api/pacientes", routerPacientes)
 app.set('view engine', 'pug')
